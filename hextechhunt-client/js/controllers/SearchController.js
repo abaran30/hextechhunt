ab645c7b9d6ca75929b0f1461a360f55eb698b2b
Adrian Baran <adrian.baran.30@gmail.com>
2016-04-27 21:33:09 -0500

    Initial commit for HextechHuntService and added more styling

--- hextechhunt-client/js/controllers/SearchController.js
+++ hextechhunt-client/js/controllers/SearchController.js
@@ -1,5 +1,6 @@
 angular.module('hextechhuntClientApp')
 .controller('SearchController',
-  function ($scope, $location) {
-
+  function ($scope, $location, HextechHuntService) {
+    $scope.availableRegions = HextechHuntService.getAvailableRegions(); // Get list of available regions to display in the view
+    $scope.selectedRegion = $scope.availableRegions[0]; // Set the default region to the first region from availableRegions
   });
---
+++ hextechhunt-client/js/services/HextechHuntService.js
@@ -0,0 +1,53 @@
+angular.module('hextechhuntClientApp')
+  .service('HextechHuntService', function($http, $q) {
+    var deferred = $q.defer();
+    var baseProxyUrl = 'http://localhost:3030';
+
+    var availableRegions = [
+      {id: 'NA', name: 'NA'},
+      {id: 'EUNE', name: 'EU Nordic & East'},
+      {id: 'EUW', name: 'EU West'},
+      {id: 'BR', name: 'Brazil'},
+      {id: 'JP', name: 'Japan'},
+      {id: 'KR', name: 'Korea'},
+      {id: 'LAN', name: 'Latin America North'},
+      {id: 'LAS', name: 'Latin America South'},
+      {id: 'OCE', name: 'Oceania'},
+      {id: 'RU', name: 'Russia'},
+      {id: 'TR', name: 'Turkey'}
+    ];
+
+    var platforms = {
+      'NA': 'NA1',
+      'EUNE': 'EUN1',
+      'EUW': 'EUW1',
+      'BR': 'BR1',
+      'JP': 'JP1',
+      'KR': 'KR',
+      'LAN': 'LA1',
+      'LAS': 'LA2',
+      'OCE': 'OC1',
+      'RU': 'RU',
+      'TR': 'TR1'
+    }
+
+    this.getAvailableRegions = function() {
+      return availableRegions;
+    };
+
+    this.getPlatformId = function(region) {
+      return platforms[region];
+    }
+
+    this.getSummoner = function(region, summonerName) {
+      $http.get(baseProxyUrl + '/api/lol/' + region + '/v1.4/summoner/by-name/' + summonerName)
+        .then(function(response) {
+          deferred.resolve(response.data);
+        })
+        .catch(function(response) {
+          deferred.reject(response);
+        });
+
+      return deferred.promise;
+    };
+  });
