a6a4739ba697cd0eb5fada1729af97417f73f678
Adrian Baran <adrian.baran.30@gmail.com>
2016-05-08 20:14:10 -0500

    Adjusted format of app.js for clean indentation [2]

--- hextechhunt-client/js/app.js
+++ hextechhunt-client/js/app.js
@@ -2,25 +2,26 @@ var hextechhuntClientApp = angular.module('hextechhuntClientApp', ['ngRoute']);
 
 // Application routing
 hextechhuntClientApp.config(['$routeProvider',
-	function ($routeProvider) {
-		$routeProvider
-			// About
-			.when('/about', {
-				templateUrl: 'views/about.html',
-				controller: 'AboutController'
-			})
-			// Results
-			.when('/results/:summonerName/:region/:summonerId/:profileIconId/:summonerLevel', {
+  function ($routeProvider) {
+    $routeProvider
+		  // About
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutController'
+      })
+      // Results
+      .when('/results/:summonerName/:region/:summonerId/:profileIconId/:summonerLevel', {
 				templateUrl: 'views/results.html',
-				controller: 'ResultsController'
-			})
-			// Search
-			.when('/search', {
-				templateUrl: 'views/search.html',
-				controller: 'SearchController'
-			})
-			// Else --> Search
-			.otherwise({
-				redirectTo: '/search'
-			});
-		}]);
+        controller: 'ResultsController'
+      })
+      // Search
+      .when('/search', {
+        templateUrl: 'views/search.html',
+        controller: 'SearchController'
+      })
+      // Else --> Search
+      .otherwise({
+        redirectTo: '/search'
+      });
+	}
+]);
